d63e3ce974bf6f44e5ee522d0757a8f7fdb35218
IrinaByck <ira002385@gmail.com>
2025-11-19 16:04:40 +0300

    Update decimal.h

--- lab2/include/decimal.h
+++ lab2/include/decimal.h
@@ -1,23 +1,29 @@
 #pragma once
+
 #include <iostream>
 #include <string>
 #include <stdexcept>
+
 class Decimal {
 public:
-Decimal();
-Decimal(const size_t & n, unsigned char t = 0);
-Decimal(const std::initializer_list< unsigned char> &t);
-Decimal(const std::string &t);
-Decimal(const Decimal& other);
-Decimal(Decimal&& other) noexcept;
-virtual ~Decimal() noexcept;
-Decimal operator+(const Decimal& other) const;
-Decimal operator-(const Decimal& other) const;
-bool operator==(const Decimal& other) const;
-bool operator>(const Decimal& other) const;
-bool operator<(const Decimal& other) const;
-void print(std::ostream& os) const;
+    Decimal();
+    Decimal(size_t n, unsigned char t = 0);
+    Decimal(const std::string &t);
+    Decimal(const Decimal& other);
+    Decimal(Decimal&& other) noexcept;
+    Decimal& operator=(const Decimal& other);
+    Decimal& operator=(Decimal&& other) noexcept;
+    ~Decimal() noexcept;
+
+    Decimal operator+(const Decimal& other) const;
+    Decimal operator-(const Decimal& other) const;
+
+    bool operator==(const Decimal& other) const;
+    bool operator<(const Decimal& other) const;
+    bool operator>(const Decimal& other) const;
+    void print(std::ostream& os) const;
+
 private:
-size_t _size;
-unsigned char* _array;
+    size_t _size;
+    unsigned char* _array;
 };
