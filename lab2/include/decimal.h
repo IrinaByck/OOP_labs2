eb493f1d80450d9c1f494d5c77218d1a0321dfed
IrinaByck <ira002385@gmail.com>
2025-11-22 01:21:55 +0300

    Update decimal.h

--- lab2/include/decimal.h
+++ lab2/include/decimal.h
@@ -1,27 +1,29 @@
 #pragma once
 
-#include <iostream>
+#include <iostream>//input//output
 #include <string>
-#include <stdexcept>
-
+#include <stdexcept>//
 class Decimal {
-public:
-    Decimal();
-    Decimal(size_t n, unsigned char t = 0);
-    Decimal(const std::string &t);
-    Decimal(const Decimal& other);
-    Decimal(Decimal&& other) noexcept;
-    Decimal& operator=(const Decimal& other);
-    Decimal& operator=(Decimal&& other) noexcept;
-    ~Decimal() noexcept;
+    public:
+    
+        Decimal();//пустой массив
+        Decimal(size_t n, unsigned char t = 0);//массив из n элементов, со значением t всех элементов
+        Decimal(const std::string &t); //массив из символов строки
 
-    Decimal operator+(const Decimal& other) const;
-    Decimal operator-(const Decimal& other) const;
+        Decimal(const Decimal& other); //копия существующего массива
+        Decimal(Decimal&& other) noexcept; //конструктор перемещения
 
-    bool operator==(const Decimal& other) const;
-    bool operator<(const Decimal& other) const;
-    bool operator>(const Decimal& other) const;
-    void print(std::ostream& os) const;
+        Decimal& operator=(const Decimal& other);        // Оператор присваивания копированием
+        Decimal& operator=(Decimal&& other) noexcept;    // Оператор присваивания перемещением
+        ~Decimal() noexcept; //очищает память, когда массив уничтожается
+        
+        Decimal operator+(const Decimal& other) const;
+        Decimal operator-(const Decimal& other) const;
+
+        bool operator==(const Decimal& other) const;
+        bool operator>(const Decimal& other) const;
+        bool operator<(const Decimal& other) const;
+        void print(std::ostream& os) const;
 
 private:
     size_t _size;
