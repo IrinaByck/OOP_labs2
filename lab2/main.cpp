75bb3624b12243445ad2e1e66742b209a2184477
IrinaByck <ira002385@gmail.com>
2025-11-22 10:09:21 +0300

    Update main.cpp

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -1,91 +1,56 @@
 #include "include/decimal.h"
 
 int main()
-
 {
-
     std::cout << std::boolalpha;
-
     std::string num1, num2;
-
     char op;
-
-    // Ввод данных
-
-    std::cout << "Введите первое число (в семеричной системе): ";
-
+    std::cout << "Введите первое число (в десятиричной системе): ";
     std::cin >> num1;
-
-    std::cout << "Введите второе число (в семеричной системе): ";
-
+    std::cout << "Введите второе число (в десятиричной системе): ";
     std::cin >> num2;
-
     std::cout << "Введите операцию из [+,-,>,<,==]: ";
-
     std::cin >> op;
 
    Decimal first(num1), second(num2);
-
     std::cout << "\nОтвет: ";
-
     switch (op) {
-
     case '+': {
-
         Decimal sum = first + second;
-
         sum.print(std::cout);
-
         break;
 
     }
 
     case '-': {
-
         Decimal diff = first - second;
-
         diff.print(std::cout);
-
         break;
 
     }
 
     case '=': {
-
         std::cout << (first == second);
-
         break;
-
     }
 
     case '<': {
-
         std::cout << (first < second);
-
         break;
-
     }
 
     case '>': {
-
         std::cout << (first > second);
-
         break;
 
     }
 
     default: {
-
         std::cerr << "Ошибка: недопустимая операция.\n";
-
         break;
-
     }
 
     }
-
     std::cout << '\n';
-
     return 0;
-
 }
