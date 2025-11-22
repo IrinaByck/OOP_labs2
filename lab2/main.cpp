e6f79b898646fc576cd499c362f28f546ba55663
IrinaByck <ira002385@gmail.com>
2025-11-22 09:57:40 +0300

    Update main.cpp

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -1,1 +1,50 @@
+#include <iostream>
+#include "include/decimal.h"
 
+int main()
+{
+    std::string num1, num2;
+    char op;
+
+    std::cout << "Введите первое число: ";
+    std::cin >> num1;
+    std::cout << "Введите второе число: ";
+    std::cin >> num2;
+    std::cout << "Введите операцию из [+,-,>,<,==]: ";
+    std::cin >> op;
+
+    Decimal first(num1), second(num2);
+
+    std::cout << "Результат: ";
+    switch (op) {
+    case '+': {
+        Decimal sum = first + second;
+        sum.print(std::cout);
+        break;
+    }
+    case '-': {
+        Decimal diff = first - second;
+        diff.print(std::cout);
+        break;
+    }
+    case '=': {
+        std::cout << (first == second);
+        break;
+    }
+    case '<': {
+        std::cout << (first < second);
+        break;
+    }
+    case '>': {
+        std::cout << (first > second);
+        break;
+    }
+    default: {
+        std::cerr << "Ошибка: недопустимая операция.\n";
+        break;
+    }
+    }
+
+    std::cout << '\n';
+    return 0;
+}
