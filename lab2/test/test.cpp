2845a3d45ae4ba93156fc7e9b4a0ee0d1fb50c2e
IrinaByck <ira002385@gmail.com>
2025-11-22 10:06:27 +0300

    Update test.cpp

--- lab2/test/test.cpp
+++ lab2/test/test.cpp
@@ -1,83 +1,82 @@
 #include <gtest/gtest.h>
+
 #include "../include/decimal.h"
 
-TEST(test_01, basic_test_set)
+TEST(test_01, basic_addition)
+
 {
-    Decimal d1("123");
-    Decimal d2("45");
-    Decimal result = d1 + d2;
-    
-    testing::internal::CaptureStdout();
-    result.print(std::cout);
-    std::string output = testing::internal::GetCapturedStdout();
-    
-    ASSERT_TRUE(output == "168");
+
+    Decimal a("12");
+
+    Decimal b("34");
+
+    Decimal result = a + b;
+
+
+    ASSERT_TRUE(a < b);
+
 }
 
-TEST(test_02, basic_test_set)
+TEST(test_02, basic_subtraction) 
+
 {
-    Decimal d1("456");
-    Decimal d2("123");
-    Decimal result = d1 - d2;
+
+    Decimal a("50");
+
+    Decimal b("30");
+
     
-    testing::internal::CaptureStdout();
-    result.print(std::cout);
-    std::string output = testing::internal::GetCapturedStdout();
-    
-    ASSERT_TRUE(output == "333");
+
+    ASSERT_TRUE(a > b);
+
 }
 
-TEST(test_03, basic_test_set)
+TEST(test_03, equality_test)
+
 {
-    Decimal d1("123");
-    Decimal d2("456");
+
+    Decimal a("100");
+
+    Decimal b("100");
+
     
-    ASSERT_TRUE(d1 < d2);
+
+    ASSERT_TRUE(a == b);
+
 }
 
-TEST(test_04, basic_test_set)
+TEST(test_04, copy_test)
+
 {
-    Decimal d1("456");
-    Decimal d2("123");
+
+    Decimal a("123");
+
+    Decimal b = a;
+
     
-    ASSERT_TRUE(d1 > d2);
+
+    ASSERT_TRUE(a == b);
+
 }
 
-TEST(test_05, basic_test_set)
-{
-    Decimal d1("123");
-    Decimal d2("123");
-    
-    ASSERT_TRUE(d1 == d2);
-}
+TEST(test_05, different_numbers)
 
-TEST(test_06, basic_test_set)
 {
-    Decimal d1("999");
-    Decimal d2("1");
-    Decimal result = d1 + d2;
-    
-    testing::internal::CaptureStdout();
-    result.print(std::cout);
-    std::string output = testing::internal::GetCapturedStdout();
-    
-    ASSERT_TRUE(output == "1000");
-}
 
-TEST(test_07, basic_test_set)
-{
-    Decimal d1("100");
-    Decimal d2("50");
-    Decimal result = d1 - d2;
+    Decimal a("999");
+
+    Decimal b("1");
+
     
-    testing::internal::CaptureStdout();
-    result.print(std::cout);
-    std::string output = testing::internal::GetCapturedStdout();
-    
-    ASSERT_TRUE(output == "50");
+
+    ASSERT_TRUE(a > b);
+
 }
 
 int main(int argc, char **argv) {
+
     testing::InitGoogleTest(&argc, argv);
+
     return RUN_ALL_TESTS();
+
 }
